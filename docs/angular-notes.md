a53fc939b9398efd3173eb136848a69f81b0a998
Greg Wiley <gcwiley@gmail.com>
2024-11-05 14:19:59 -0500

    additional code added

--- docs/angular-notes.md
+++ docs/angular-notes.md
@@ -9,3 +9,10 @@
 
 ### Angular vs React vs Vue
 - Angular - supported by Google
+
+#### pathMatch
+
+The 'pathMatch' property is used to control how Angular router matches URLs to 
+routes
+
+The setting 'pathMatch: full', you ensure that a route only matches when the entire URL exactly matches the route path.
--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,23 +1,9 @@
-// about page
 export { AboutPageComponent } from './about-page/about-page.component';
-
-// admin page
 export { AdminPageComponent } from './admin-page/admin-page.component';
-
-// album pages
 export { AlbumCreatePageComponent, AlbumDetailsPageComponent, AlbumGridPageComponent } from '../pages/album-pages';
-
-// hero pages
+export { ErrorPageComponent } from './error-page/error-page.component';
 export { HeroCreatePageComponent, HeroDetailsPageComponent, HeroGridPageComponent } from '../pages/hero-pages';
-
-// home page
 export { HomepageComponent } from './homepage/homepage.component';
-
-// image gallery page
 export { ImageGalleryPageComponent } from './image-gallery-page/image-gallery-page.component';
-
-// not found page
 export { NotFoundPageComponent } from './not-found-page/not-found-page.component';
-
-// sign in page
 export { SigninPageComponent } from './signin-page/signin-page.component';

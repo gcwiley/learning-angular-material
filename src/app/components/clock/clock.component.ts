2a5fa84e30761dcb2dbf78fc543b7039fcf16539
Greg Wiley <33140616+gcwiley@users.noreply.github.com>
2025-04-14 16:12:08 -0400

    Update clock.component.ts

--- src/app/components/clock/clock.component.ts
+++ src/app/components/clock/clock.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
 
 @Component({
   selector: 'app-clock',
@@ -7,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './clock.component.scss'
 })
 export class ClockComponent {
+    // stores the current time as a Date object
+  public currentTime: Date = new Date();
+  private timeSubscription: Subscription | null = null;
 
+  // define time zones in an array
+  public timeZones = [
+    { label: 'Local', offset: 'GMT-4' },
+    { label: 'Zulu', offset: 'GMT' },
+     { label: 'Tel Aviv', offset: 'GMT+2' },
+     { label: 'Kyiv', offset: 'GMT+2' 
+    ]
+
+  // set up the clock update interval of 1 second
+  ngOnInit(): void {
+    this.timeSubscription = internal(1000).subscribe(() => {
+      this.currentTime = new Date();
+    });
+  }
+
+  // clean up the subscription when the component is destoyed
+  ngOnDestroy(): void {
+    this.timeSubscription?.unsubscribe();
+  }
 }
+
+  
